5d102cde9bdde8db2a4b471c5202d1937aba7f40
jrmaiafs <67764559+jrmaiafs@users.noreply.github.com>
2021-10-25 08:57:28 -0300

    added specification route create and find by name

---
+++ src/routes/especification.routes.ts
@@ -0,0 +1,21 @@
+import { Router } from "express";
+
+import { SpecificationsRepository } from "../modules/cars/repositories/SpecificationsRepository";
+import { CreateSpecificationService } from "../modules/cars/service/CreateSpecificationService";
+
+const specifationsRoutes = Router();
+const specificationsRepository = new SpecificationsRepository();
+
+specifationsRoutes.post("/", (req, res) => {
+  const { name, description } = req.body;
+
+  const createSpecificationService = new CreateSpecificationService(
+    specificationsRepository
+  );
+
+  createSpecificationService.execute({ name, description });
+
+  return res.status(201).send();
+});
+
+export { specifationsRoutes };
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 
 import { categoriesRoutes } from "./routes/categories.routes";
+import { specifationsRoutes } from "./routes/especification.routes";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/categories", categoriesRoutes);
+app.use("/specifications", specifationsRoutes);
 
 app.listen(3333, () => console.log("Server is running!"));
