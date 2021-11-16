822c057d5602794e4ea2feee6c97ef497cf75232
maiajrs <jr.maiafs1@gmail.com>
2021-11-16 09:56:35 -0300

    create initial tests and implemented car classes

---
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -0,0 +1,32 @@
+import { Column, CreateDateColumn, PrimaryColumn } from 'typeorm';
+import {v4 as uuidV4} from 'uuid'
+
+class Car {
+    // @PrimaryColumn()
+    id: string;
+    // @Column()
+    name: string;
+    // @Column()
+    description: string;
+    // @Column()
+    daily_rate: number;
+    // @Column()
+    available: boolean;
+    // @Column()
+    license_plate: string;
+    // @Column()
+    fine_amount: number;
+    // @Column()
+    brand: string;
+    // @Column()
+    category: string;
+    // @CreateDateColumn()
+    created_at: Date;
+    constructor() {
+        if (!this.id) {
+            this.id = uuidV4()
+        }
+    }
+}
+
+export {Car}
